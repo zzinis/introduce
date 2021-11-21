45d5b66f2b1f5e82a44e9c4d15d7d4604ce128e3
zzinis <lizzyin@naver.com>
2021-11-21 20:01:35 +0900

    swiper btn 추가

--- js/main.js
+++ js/main.js
@@ -1,1 +1,26 @@
-const swiper = new Swiper("#wrap");
+const swiper = new Swiper("#wrap",{
+    loop: true,
+    sliderPerView: "auto",
+    centeredSlides: true,
+    spaceBetween: 50,
+    mousewheel: true,
+
+    navigation:{
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+
+    },
+    pagination:{
+        el: ".swiper-pagination",
+        type: "bullets",
+        clickable: true
+    },
+    // effect :"coverflow",
+    // coverflowEffect:{
+    //     rotate:50,
+    //     stretch: -100,
+    //     depth: 400,
+    //     slideShadow: false
+    // }
+
+});
